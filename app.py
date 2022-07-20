ec1a56cd61aba8f6a516a8f8dcc8f4a0ef7197c1
Jeff McElhannon <jeff.mcelhannon@capitalone.com>
2022-07-20 07:01:26 -0400

    continued refactor

--- app.py
+++ app.py
@@ -1,47 +1,44 @@
 from bs4 import BeautifulSoup
 import click
 
+import logging
+import os
 import time
 
-from library import compute, driver
+from library import compute, driver, insider
+
+LOGLEVEL = os.environ.get('LOGLEVEL', 'WARNING').upper()
+logging.basicConfig(level=LOGLEVEL)
 
 @click.command()
 @click.option('--url', required=True, type=str, help="Stern Insider Leaderboard **Kiosk** URL")
-def main(url):
+@click.option('--num-pages', required=True, default=2, type= int, help="Number of pages in the leaderboard")
+def main(url, num_pages):
     scores = dict()
     player_names = list()
      
-    webdriver = driver.get_webdriver()
-    webdriver.get(url)
-    element = driver.wait_for_element_id(webdriver, "shuffle")
+    webdriver = driver.Driver()
+    logging.debug("loading leaderboard")
+    webdriver.get_webdriver().get(url)
+    logging.debug("waiting for leaderboard to show")
+    webdriver.wait_for_element_id("shuffle")
     time.sleep(1)
 
-    # run through this twice so we capture the second page of the board
-    for x in range(0, 2):
-        html = webdriver.page_source
-        soup = BeautifulSoup(str(html), 'html.parser')
-        div = soup.find("div", {"id": "shuffle"})
-        ul = div.find('ul')
-        cards = ul.find_all('li', recursive=False)
-        for card in cards:
-            player_names = list()
-            leaderboard_header = card.find("div", {"class": "leaderboard-header"})
-            title=leaderboard_header.find('p')
-            game_name = title.contents[0]
-            # prevent duplicate scores from being added
-            if game_name in scores:
-                pass
-            players = card.find_all('li')
-            for player in players:
-                name = player.find('p')
-                player_name = name.contents[0]
-                # don't add dan's scores
-                if player_name.lower() != "flex_bt":
-                    player_names.append(player_name)
+    sterninsider = insider.Insider()
+    
+    logging.debug(f"running through {num_pages} leaderboard pages")
+    for x in range(0, num_pages):
+        html = webdriver.get_webdriver().page_source
+        sterninsider.parse_leaderboard(html)
+        wait_counter = 0
+        while webdriver.get_webdriver().page_source == html:   
+            time.sleep(1)
+            wait_counter += 1
+            if wait_counter == 20:
+                logging.warning("wait timeout reached")
+                break
 
-            scores[game_name] = player_names
-
-        time.sleep(9)
+    scores = sterninsider.get_scores()
     computer = compute.Compute()
     total_scores = computer.get_scores(scores)
     print(total_scores)
--- library/driver.py
+++ library/driver.py
@@ -4,17 +4,28 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+import logging
 import os
 
-def get_webdriver():
-    os.environ['WDM_SSL_VERIFY'] = '0'
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option("detach", True)
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+class Driver:
+    def __init__(self):
+        self.driver=self.setup_webdriver()
 
-    return driver
+    def setup_webdriver(self):
+        os.environ['WDM_SSL_VERIFY'] = '0'
+        options = webdriver.ChromeOptions()
+        # options.add_experimental_option("detach", True)
+        options.add_argument("--headless")
+        logging.debug("starting webdriver")
+        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+        logging.debug("got webdriver")
+        return driver
 
-def wait_for_element_id(thedriver, thename):
-    return WebDriverWait(thedriver, 10).until(
-            EC.visibility_of_element_located((By.ID, thename))
+    def get_webdriver(self):
+        return self.driver
+
+
+    def wait_for_element_id(self, thename):
+        return WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located((By.ID, thename))
         )
