38be8d0b86c128a6dafd480bc519e5de4e96483e
FlexBT <109229702+FlexBT@users.noreply.github.com>
2022-12-04 09:51:36 -0500

    Update script.sh
    changed link to Dec

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ git stash push --include-untracked
 git merge -X ours origin/main --no-commit
 
 /usr/local/bin/pipenv install --skip-lock
-/usr/local/bin/pipenv run python app.py --url 'https://insider.sternpinball.com/kiosk/LxjKr-pMD-SPt'
+/usr/local/bin/pipenv run python app.py --url 'https://insider.sternpinball.com/kiosk/bTzC-PtKN-gfP'
 git add -A
 timestamp=$(date +%s)
 git commit -m timestamp=$(date +%s)
