8721bdc88ce183ca2ed9f1eff5224193f4ef431b
FlexBT <109229702+FlexBT@users.noreply.github.com>
2023-11-02 13:01:40 -0400

    Update script.sh
    review of last months winner changed url

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ git stash push --include-untracked
 git merge -X ours origin/main --no-commit
 
 /usr/local/bin/pipenv install --skip-lock
-/usr/local/bin/pipenv run python app.py --url 'https://insider.sternpinball.com/kiosk/LSZ-EsbiQ-yVr'
+/usr/local/bin/pipenv run python app.py --url 'https://insider.sternpinball.com/kiosk/LSZ-EsbiQ-yVr?isPast=true'
 git add -A
 timestamp=$(date +%s)
 git commit -m timestamp=$(date +%s)
