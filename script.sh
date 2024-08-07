6f91ea920ed447ff90267783582a5f1600159302
FlexBT <109229702+FlexBT@users.noreply.github.com>
2024-08-07 11:34:41 -0400

    Update script.sh
    updated to August

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ git stash push --include-untracked
 git merge -X ours origin/main --no-commit
 
 /usr/local/bin/pipenv install --skip-lock
-/usr/local/bin/pipenv run python app.py --url 'https://insider.sternpinball.com/kiosk/MiQL-XKQx-yPsP?portrait'
+/usr/local/bin/pipenv run python app.py --url 'https://insider.sternpinball.com/kiosk/cpAsW-PGPT-oUPG?portrait'
 git add -A
 timestamp=$(date +%s)
 git commit -m timestamp=$(date +%s)
